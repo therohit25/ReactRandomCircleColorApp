1733bed1eb4d8168fd655a8bd0a75b4724b5df8f
therohit25 <rohitghadage40@gmail.com>
2023-07-13 23:02:31 +0530

    Updates

---
+++ precache-manifest.bcc16160e15587c3f2aa6fc455eead68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "35a7087c7113a4a0e78d",
+    "url": "/ReactRandomCircleColorApp/static/js/main.35a7087c.chunk.js"
+  },
+  {
+    "revision": "03c018fb6e914630103e",
+    "url": "/ReactRandomCircleColorApp/static/js/1.03c018fb.chunk.js"
+  },
+  {
+    "revision": "5904e87ccf52d18040c4",
+    "url": "/ReactRandomCircleColorApp/static/css/2.b0e2689e.chunk.css"
+  },
+  {
+    "revision": "5904e87ccf52d18040c4",
+    "url": "/ReactRandomCircleColorApp/static/js/2.5904e87c.chunk.js"
+  },
+  {
+    "revision": "7d74defe9ae77153a838",
+    "url": "/ReactRandomCircleColorApp/static/js/runtime~main.7d74defe.js"
+  },
+  {
+    "revision": "c9e5d63dbc43fac21c5927b630a2bfec",
+    "url": "/ReactRandomCircleColorApp/index.html"
+  }
+];
